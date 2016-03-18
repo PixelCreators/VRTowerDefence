5b3f0dd7ade9797ccdcfea22360dbc9d8ce4e2a4
Artur <artur1947@o2.pl>
2016-03-19 00:08:51 +0100

    Added PlayerController

---
+++ VRTowerDefence/Assets/Scripts/PlayerShooter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooter : MonoBehaviour {
+
+    public GameObject shot;
+
+    private bool gotHit;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit hit;
+        Ray landingRay = new Ray(transform.position, Vector3.down);
+
+        if(!gotHit)
+        {
+            if(Physics.Raycast)
+        }
+	}
+}
