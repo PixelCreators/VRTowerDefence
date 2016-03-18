e3d874278f4806f07976d5e3a6510b24a6f361c8
Mateusz Pusty <mateusz.pusty@pixelcreators.pl>
2016-03-18 23:43:28 +0100

    Made some changes

---
+++ VRTowerDefence/Assets/Teleporter.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour
+{
+    public float lookTime = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        StartCoroutine(WaitTime());
+    
+    }
+
+    public void SetGazedOff()
+    {
+        StopCoroutine(WaitTime());
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void GazedClick()
+    {
+
+    }
+
+    public IEnumerator WaitTime()
+    {
+        var currentLookTime = 0.0f;
+        while (true)
+        {
+            yield return new WaitForSeconds(lookTime / 60);
+            currentLookTime += lookTime / 60;
+            if (currentLookTime == lookTime)
+                break;
+        }
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+}
